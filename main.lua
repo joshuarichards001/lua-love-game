f6429780d37d71226a875a08b47972b0ce1e8a35
Josh Richards <joshuarichards001@gmail.com>
2024-12-22 18:04:48 +1300

    Add background

--- main.lua
+++ main.lua
@@ -1,29 +1,40 @@
 function love.load()
-  player = {}
-  player.x = 400
-  player.y = 200
-  player.speed = 3
-  player.sprite = love.graphics.newImage("assets/Player/Player.png")
-  player.sprite:setFilter("nearest", "nearest")
-  player.quad = love.graphics.newQuad(0, 0, 32, 32, player.sprite:getDimensions())
-  player.scale = 4
+  TileSize = 32
+
+  Scale = 4
+
+  Background = love.graphics.newImage("assets/Tiles/Grass_Middle.png")
+
+  Player = {}
+  Player.x = 400
+  Player.y = 200
+  Player.speed = 3
+  Player.sprite = love.graphics.newImage("assets/Player/Player.png")
+  Player.sprite:setFilter("nearest", "nearest")
+  Player.quad = love.graphics.newQuad(0, 0, TileSize, TileSize, Player.sprite:getDimensions())
 end
 
 function love.update(dt)
   if love.keyboard.isDown("right") then
-    player.x = player.x + player.speed
+    Player.x = Player.x + Player.speed
   end
   if love.keyboard.isDown("left") then
-    player.x = player.x - player.speed
+    Player.x = Player.x - Player.speed
   end
   if love.keyboard.isDown("down") then
-    player.y = player.y + player.speed
+    Player.y = Player.y + Player.speed
   end
   if love.keyboard.isDown("up") then
-    player.y = player.y - player.speed
+    Player.y = Player.y - Player.speed
   end
 end
 
 function love.draw()
-  love.graphics.draw(player.sprite, player.quad, player.x, player.y, 0, player.scale, player.scale)
+  for y = 0, 100, 1 do
+    for x = 0, 100, 1 do
+      love.graphics.draw(Background, TileSize * x, TileSize * y, 0, Scale, Scale)
+    end
+  end
+
+  love.graphics.draw(Player.sprite, Player.quad, Player.x, Player.y, 0, Scale, Scale)
 end
